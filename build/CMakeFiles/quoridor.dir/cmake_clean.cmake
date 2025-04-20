2040366f8e60cfff64a1b4542f6777f0bfac6c93
Iram Liu <il233@cornell.edu>
2025-04-20 18:00:54 -0400

    fixed some cpp bugs

---
+++ build/CMakeFiles/quoridor.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quoridor.dir/MCTS.cpp.o"
+  "CMakeFiles/quoridor.dir/MCTS.cpp.o.d"
+  "CMakeFiles/quoridor.dir/game_state_tuple.cpp.o"
+  "CMakeFiles/quoridor.dir/game_state_tuple.cpp.o.d"
+  "CMakeFiles/quoridor.dir/main.cpp.o"
+  "CMakeFiles/quoridor.dir/main.cpp.o.d"
+  "quoridor"
+  "quoridor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quoridor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
