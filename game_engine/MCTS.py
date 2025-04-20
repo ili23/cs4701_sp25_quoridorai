d43336b4d52f240ac9f0934a9054f1f63b1dd896
Iram Liu <il233@cornell.edu>
2025-04-20 16:14:39 -0400

    changed mcts

--- game_engine/MCTS.py
+++ game_engine/MCTS.py
@@ -5,12 +5,17 @@ from queue import Queue
 from utility import argmax
 from game_state_tuple import get_possible_moves, apply_move, is_terminal, check_winner, get_current_player
 
+POSSIBLE_MOVES = {}
+def access_cache_or_update(state):
+    if state not in POSSIBLE_MOVES:
+        POSSIBLE_MOVES[state] = get_possible_moves(state)
+    return POSSIBLE_MOVES[state]
 
 def rollout(state, max_steps=100):
     t = 0
     while not is_terminal(state) and t < max_steps:
         # Heuristic: prefer moves that advance toward goal
-        moves = get_possible_moves(state)
+        moves = access_cache_or_update(state)
         current_player = get_current_player(state)
         
         # Prioritize pawn moves that advance toward goal
@@ -20,7 +25,7 @@ def rollout(state, max_steps=100):
             # Sort moves by distance to goal
             pawn_moves.sort(key=lambda m: abs(m[1][0] - goal_row))
             # 70% chance to pick advancing move, 30% random
-            if random.random() < 0.7:
+            if random.random() < 1.0:
                 a = pawn_moves[0]  # Choose move closest to goal
             else:
                 a = random.choice(moves)
@@ -33,19 +38,22 @@ def rollout(state, max_steps=100):
     return check_winner(state)
 
 class GameTree:
-    def __init__(self, start_state, parent=None):
+    def __init__(self, start_state, parent=None, state_cache={}):
         self.state = start_state
-        self.possible_actions = get_possible_moves(self.state)
         self.children = None
         self.parent = parent
-        self.n = 0
-        self.w = 0
-
+        self.state_cache = state_cache
+        if self.state not in self.state_cache:
+            self.state_cache[self.state] = {
+                "n": 0,
+                "w": 0,
+            }
+        
     def value(self):
-        if self.n == 0:
+        if self.state_cache[self.state]["n"] == 0:
             return np.inf
         c = np.sqrt(2)
-        return (self.w / self.n) + c * np.sqrt(np.log(self.parent.n) / self.n)
+        return (self.state_cache[self.state]["w"] / self.state_cache[self.state]["n"]) + c * np.sqrt(np.log(self.state_cache[self.parent.state]["n"]) / self.state_cache[self.state]["n"])
     
     def expand(self):
         if is_terminal(self.state):
@@ -53,19 +61,19 @@ class GameTree:
         else:
             if self.children is None:
                 self.backprop(rollout(self.state))
-                self.children = [GameTree(apply_move(self.state, a), parent=self) for a in self.possible_actions]
+                self.children = [GameTree(apply_move(self.state, a), parent=self, state_cache=self.state_cache) for a in access_cache_or_update(self.state)]
             else:
                 values = [child.value() for child in self.children]
                 self.children[np.argmax(values)].expand()
     
     def backprop(self, winner):
-        self.n += 1
+        self.state_cache[self.state]["n"] += 1
         if winner is not None:  # There is a winner
             # The previous player is the opposite of current player in this state
             previous_player = 1 - get_current_player(self.state)
             # If the winner is the previous player, increment wins
             if previous_player == winner:
-                self.w += 1
+                self.state_cache[self.state]["w"] += 1
         
         if self.parent is not None:
             self.parent.backprop(winner)
@@ -86,43 +94,23 @@ class GameTree:
         return None  # Return None if child not found
 
 class Agent:
-    def __init__(self, iters=10):
+    def __init__(self, iters = 100):
         self.search_depth = iters
         self.tree = None
-        self.move_cache = {}  # State hash -> list of possible moves
     
-    def get_cached_moves(self, state):
-        """Get cached moves for a state or calculate and cache them"""
-        state_hash = hash(state)
-        if state_hash in self.move_cache:
-            print("Found moves in cache")
-            return self.move_cache[state_hash]
-        
-        moves = get_possible_moves(state)
-        self.move_cache[state_hash] = moves
-        return moves
-    
-    def select_move(self, state, update_tree=True):
+    def select_move(self, state):
         assert not is_terminal(state)
-
-        # Update tree so that root node has current board state
-        if not (self.tree and update_tree):
+        if self.tree is None: 
             self.tree = GameTree(state)
-        else:
-            # If possible, look in old tree for cur state
-            self.tree = self.tree.find_child(state)
-            if self.tree is None:
-                self.tree = GameTree(state)
-
-        # Use cached moves for the tree
-        self.tree.possible_actions = self.get_cached_moves(state)
+        elif self.tree.state != state:
+            self.tree = GameTree(state, state_cache=self.tree.state_cache)
 
         for _ in range(self.search_depth):
             self.tree.expand()
+        
+        possible_moves = access_cache_or_update(state)
+        cache = self.tree.state_cache
 
-        # Get the best child based on win rate
-        best_child_idx = argmax(self.tree.children, key=lambda tree: 0 if tree.n == 0 else tree.w / tree.n)
-        return self.get_cached_moves(state)[best_child_idx]
+        return possible_moves[argmax(self.tree.children, key=lambda tree: 0 if cache[tree.state]["n"] == 0 else cache[tree.state]["w"] / cache[tree.state]["n"])]          
     
-
     
