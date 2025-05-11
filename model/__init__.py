0efb4a9b7cd174a13b34e698ca171a7618a8c6f4
Iram Liu <il233@cornell.edu>
2025-05-11 09:57:34 -0400

    fixed gitignore

--- model/__init__.py
+++
@@ -1,3 +0,0 @@
-from model.cnn import QuoridorFeatureExtractor, QuoridorDataset, QuoridorCNN, QuoridorLightningModule
-from model.trainer import QuoridorTrainer
-from model.evaluator import QuoridorEvaluator
