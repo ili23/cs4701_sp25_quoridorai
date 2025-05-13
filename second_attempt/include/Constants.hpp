0beb3e7f2e598295cf466e23ef9f41680609a9d8
Mason Raffo <m-raffo@users.noreply.github.com>
2025-05-12 23:37:32 -0400

    Consolidate constants

---
+++ second_attempt/include/Constants.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+constexpr int kBoardSize = 5;
+
+constexpr int kStartingFences = 7;
+
+constexpr int kMaxMoves = 15;
+
+constexpr int p1_start_first = 0;
+constexpr int p1_start_second = 2;
+constexpr int p2_start_first = 4;
+constexpr int p2_start_second = 2;
+
+constexpr int kRandomMovesCount = 6;
+constexpr int kGameCount = 1;
